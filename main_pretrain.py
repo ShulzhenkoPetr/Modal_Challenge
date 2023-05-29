ca260b7bab4e3ca3948cc72e788057aa941691d4
ShulzhenkoPetr <petro.shulzhenko@gmail.com>
2023-05-29 09:35:48 +0200

    changed all model_without_ddp on model

--- main_pretrain.py
+++ main_pretrain.py
@@ -200,7 +200,7 @@ def main(args):
     #     model_without_ddp = model.module
 
     # following timm: set wd as 0 for bias and norm layers
-    param_groups = optim_factory.add_weight_decay(model_without_ddp, args.weight_decay)
+    param_groups = optim_factory.add_weight_decay(model, args.weight_decay)
     optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
     print(optimizer)
     loss_scaler = NativeScaler()
@@ -250,7 +250,7 @@ def main(args):
         )
         if args.output_dir:
             misc.save_model(
-                args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
+                args=args, model=model, model_without_ddp=model, optimizer=optimizer,
                 loss_scaler=loss_scaler, epoch=epoch)
 
         log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
